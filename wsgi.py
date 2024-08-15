36a9b7a13f5f60eb8317d76926238bdb784a48cf
Luis Guilherme Rampaso <luis_hisse@hotmail.com>
2024-08-15 17:00:08 -0300

    fix: added port variable to heroku's service

--- wsgi.py
+++ wsgi.py
@@ -8,4 +8,4 @@ else:
 
 # Adicionada rota 0.0.0.0 para não haver erros de conexão no contêiner
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
