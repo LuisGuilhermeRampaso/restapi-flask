e034c42a3fbf17333386be04a5b98fbe050980ff
Luis Guilherme Rampaso <luis_hisse@hotmail.com>
2024-08-16 14:07:46 -0300

    fix: testing code changes

--- tests/test_application.py
+++ tests/test_application.py
@@ -31,15 +31,15 @@ class TestApplication():
 
     def test_get_users(self, client):
         response = client.get('/users')
-        assert response.status_code == 500
+        assert response.status_code == 200
 
     def test_post_user(self, client, valid_user, invalid_user):
         response = client.post('/user', json=valid_user)
-        assert response.status_code == 500
+        assert response.status_code == 200
         assert b"successfully" in response.data
 
         response = client.post('/user', json=invalid_user)
-        assert response.status_code == 500
+        assert response.status_code == 400
         assert b"invalid" in response.data
 
     def test_get_user(self, client, valid_user, invalid_user):
